a0eb4758eab4493fe588054998e21f899af3c17b
Simone Franchina <simone.franchina@me.com>
2020-03-27 10:00:55 +0100

    base bootstrap layout

--- src/wp-theme/functions.php
+++ src/wp-theme/functions.php
@@ -120,15 +120,15 @@ add_action( 'widgets_init', 'bb_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function bb_theme_scripts() {
-	wp_enqueue_style( 'bb_theme-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bb_theme-style', get_template_directory_uri() . '/assets/styles/style.min.css');
 
-	wp_enqueue_script( 'bb_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	//wp_enqueue_script( 'bb_theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'bb_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	//wp_enqueue_script( 'bb_theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	//if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	//	wp_enqueue_script( 'comment-reply' );
+	//}
 }
 add_action( 'wp_enqueue_scripts', 'bb_theme_scripts' );
 
